ac48552c273292d16f90e959a1954b383bccb071
graus85 <graus85@gmail.com>
2025-09-28 23:42:05 +0200

    Create More.tsx

---
+++ src/pages/More.tsx
@@ -0,0 +1,8 @@
+export default function More() {
+  return (
+    <div className="card">
+      <h2>Impostazioni</h2>
+      <p>Qui metteremo dark mode, preferenze, backup/export (step 3-4).</p>
+    </div>
+  );
+}
